e91400e15ce83b752723b2f2548afc1a50e4ea6a
z.heydari <heydari.z1990@gmail.com>
2025-01-18 14:47:00 +0330

    Add Koin for implementing DI

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -30,10 +30,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     implementation(libs.androidx.core.ktx)
@@ -45,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
